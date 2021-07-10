9c0379835738c251a3ea8c52ebce6d5c048d88cb
Carlos Morales <carlosjose006@gmail.com>
2021-07-10 12:30:26 -0400

    Metodos que devuelven un valor

---
+++ MetodosConParametros/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MetodosConParametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num1, num2;
+            Console.WriteLine("Digite el primer numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite el segundo numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"El resultado de la suma es: {suma(num1, num2)}");
+        }
+
+        //static int suma(int num1, int num2) => num1 + num2;
+
+        static int suma(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            return result;
+                
+        }
+    }
+
+    
+}
