88af01110e87c35f0bab5596f5fd231e57c326f4
Carlos Morales <carlosjose006@gmail.com>
2021-07-11 23:07:22 -0400

    Practicando con el condicional if

---
+++ CondicionalIF/CondicionalIF/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("Cual es tu edad?");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad < 18) Console.WriteLine("Eres menor de edad");
+            else Console.WriteLine("Eres mayor de edad");
+        }
+    }
+}
