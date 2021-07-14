7d2fb3a0d28b3f7316d7bef352442145d940a817
Carlos Morales <carlosjose006@gmail.com>
2021-07-13 23:16:26 -0400

    Practicando el condicional switch y el bucle While

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el numero de mes");
+
+            int mes = Convert.ToInt32(Console.ReadLine());
+
+            switch (mes)
+            {
+                case 1:
+                    Console.WriteLine("El mes seleccionado es Enero");
+                    break;
+                case 2:
+                    Console.WriteLine("El mes seleccionado es Febrero");
+                    break;
+                case 3:
+                    Console.WriteLine("El mes seleccionado es Marzo");
+                    break;
+                case 4:
+                    Console.WriteLine("El mes seleccionado es Abril");
+                    break;
+                case 5:
+                    Console.WriteLine("El mes seleccionado es Mayo");
+                    break;
+                case 6:
+                    Console.WriteLine("El mes seleccionado es Junio");
+                    break;
+                case 7:
+                    Console.WriteLine("El mes seleccionado es Julio");
+                    break;
+                case 8:
+                    Console.WriteLine("El mes seleccionado es Agosto");
+                    break;
+                case 9:
+                    Console.WriteLine("El mes seleccionado es Septiembre");
+                    break;
+                case 10:
+                    Console.WriteLine("El mes seleccionado es Octubre");
+                    break;
+                case 11:
+                    Console.WriteLine("El mes seleccionado es Noviembre");
+                    break;
+                case 12:
+                    Console.WriteLine("El mes seleccionado es Diciembre");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ bucleWhile/bucleWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace bucleWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numero = new Random();
+            int numeroAleatorio = numero.Next(0, 100);
+
+            Console.WriteLine("Ingrese el numero");
+            int numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+            int contador = 1;
+
+            while (numeroUsuario != numeroAleatorio)
+            {
+
+                if (numeroUsuario > numeroAleatorio) { Console.WriteLine("El numero ingresado es mayor");
+                  Console.WriteLine("Intententalo de nuevo con otro numero");
+                  numeroUsuario = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (numeroUsuario < numeroAleatorio) { Console.WriteLine("El numero ingresado es menor");
+
+                    Console.WriteLine("Intententalo de nuevo con otro numero");
+                    numeroUsuario =Convert.ToInt32(Console.ReadLine());
+
+                }
+                contador++;
+                
+            }
+            Console.WriteLine("Felicidades, lo lograste");
+            Console.WriteLine($"Lo has logrado a los {contador} intentos");
+        }
+    }
+}
