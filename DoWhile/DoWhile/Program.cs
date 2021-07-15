9a56674a07f6637f5f5e32f2fc578f4af3fcbeb1
Carlos Morales <carlosjose006@gmail.com>
2021-07-14 21:25:11 -0400

    Practicando el bucle Do While y la excepciones con Try catch

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numero = new Random();
+            int numeroAleatorio = numero.Next(0, 100);
+
+
+            int numeroUsuario;
+
+            int contador = 0;
+
+
+            do
+            {
+                contador++;
+                Console.WriteLine("Ingrese el numero");
+                try {
+                    numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al digitar el numero, se tomo como un 0");
+                    Console.WriteLine(ex.Message);
+                    numeroUsuario = 0;
+                }
+                
+
+                if (numeroUsuario > numeroAleatorio)
+                {
+                    Console.WriteLine("El numero ingresado es mayor");
+                }
+                else if (numeroUsuario < numeroAleatorio)
+                {
+                    Console.WriteLine("El numero ingresado es menor");
+                }
+                
+
+            } while (numeroUsuario != numeroAleatorio);
+            Console.WriteLine("Felicidades, lo lograste");
+            Console.WriteLine($"Lo has logrado a los {contador} intentos");
+        }
+    }
+}
